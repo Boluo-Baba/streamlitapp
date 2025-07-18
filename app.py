2c58c37112b9af9b7fc8f35ab5b1dd7d72d84726
Boluo-Baba <1040239294@qq.com>
2025-07-18 22:42:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -513,10 +513,8 @@ elif example:
                          hide_index=True)
             st.markdown("**EOZ is decentered(EOZ% < 0.9502 or DEOZ > 1.0975)! K values reported by EOZ merging method is recommanded! BTW, default K values from smoothed corneal topography is also given.**")
 
-            st.table(pd.DataFrame({'A': ['default', 'EOZ merging method'], 'B': [f'K1: {r1[3]}D @ {r1[2]}°         K2: {r1[1]}D @ {r1[0]}°', f'K1: {r2[3]} D @ {r2[2]}°         K2: {r2[1]} D @ {r2[0]}°'],
-                                   'C': ['', "✅"]}).values)
-
-            df = pd.DataFrame([[1, 2], [3, 4], [5, 6]])
+            df = pd.DataFrame({'A': ['default', 'EOZ merging method'], 'B': [f'K1: {r1[3]}D @ {r1[2]}°         K2: {r1[1]}D @ {r1[0]}°', f'K1: {r2[3]} D @ {r2[2]}°         K2: {r2[1]} D @ {r2[0]}°'],
+                               'C': ['', "✅"]})
             html = "<table style='border-collapse: collapse;'>"
             for row in df.values:
                 html += "<tr>"
